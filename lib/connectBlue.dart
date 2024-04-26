a411f57f6717bcabe77e29771beb2ac29a9ab1b5
josecoria20 <jose.coria@popatelier.net>
2024-04-26 11:51:19 -0600

    Update app

--- lib/connectBlue.dart
+++ lib/connectBlue.dart
@@ -1,9 +1,7 @@
 import 'dart:async';
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:maletin_iqos/videoMain.dart';
 
 class ConnectBlue extends StatefulWidget {
   const ConnectBlue({ Key? key }) : super(key: key);
@@ -15,56 +13,53 @@ class ConnectBlue extends StatefulWidget {
 class _ConnectBlueState extends State<ConnectBlue> {
   StreamSubscription<List<ScanResult>>? scanSubscription;
   BluetoothDevice? device;
-  String trama = '-1,2,200,255,70,4';
 
   @override
   void initState() {
     super.initState();
-    iniciarEscaneo();
+    //Se inicia con el método startScan
+    _startScan();
   }
 
-  void iniciarEscaneo() async {
-    scanSubscription = FlutterBluePlus.onScanResults.listen((results) async {
-      for (ScanResult result in results) {
-        print('Escanenado');
-        if (result.device.advName == 'Test2') {
-          try {
-            scanSubscription?.cancel();
-            await FlutterBluePlus.stopScan();
-            device = result.device;
-            // Call connect() only once
-            await device?.connect();
-            sendData();
-            print('Conectado a ${device?.advName}');
-            setState(() {}); // Trigger a UI update
-            break;
-          } catch (error) {
-            print('Error connecting: $error');
-            // Handle connection errors appropriately
-          }
+  void _startScan() async {
+  scanSubscription = FlutterBluePlus.onScanResults.listen((results) async {
+    for (ScanResult result in results) {
+      //Los resultados del escaneo se filtran hasta encontrar el dispositivo
+      if (result.device.advName == 'ESP32IQOS') {
+        try {
+          scanSubscription?.cancel();
+          //Método para detener el escaneo
+          await FlutterBluePlus.stopScan();
+          //Se asigna a la variable local el dispositivo
+          device = result.device;
+          //Método para conectar el dispositivo
+          await device!.connect();
+          print('Conectado a ${device!.advName}');
+          changeLayout(device!); // Aquí llamamos a la función para cambiar el layout
+          break;
+        } catch (error) {
+          print('Error connecting: $error');
         }
       }
-    });
+    }
+  });
+  await FlutterBluePlus.adapterState
+      .where((val) => val == BluetoothAdapterState.on)
+      .first;
+  await FlutterBluePlus.startScan(
+    withNames: ["ESP32IQOS"],
+    timeout: const Duration(seconds: 30),
+  );
+}
 
-    await FlutterBluePlus.adapterState.where((val) => val == BluetoothAdapterState.on).first;
-    await FlutterBluePlus.startScan(
-      withNames: ["Test2"],
-      timeout: const Duration(seconds: 1500),
-    );
-  }
-  void sendData() async {
-    List<BluetoothService> services = await device!.discoverServices();
-    services.forEach((service) async {
-        // Reads all characteristics
-      var characteristics = service.characteristics;
-      for(BluetoothCharacteristic c in characteristics) {
-          if (c.properties.read) {
-              List<int> value = await c.read();
-              final receivedData = String.fromCharCodes(value);
-              print('Se recibio: $receivedData');
-          }
-      }
-    });
+
+  changeLayout(BluetoothDevice d){
+    Navigator.pushAndRemoveUntil<void>(
+        context,
+        MaterialPageRoute<void>(
+          builder: (BuildContext context) => VideoMain(signalBlue: d),
+        ),
+        (route) => false);
   }
 
   @override
@@ -75,15 +70,24 @@ class _ConnectBlueState extends State<ConnectBlue> {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
+    return const MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Conexión automática'),
-        ),
         body: Center(
-          child: Text(device != null ? 'Conectado a ${device?.advName}' : 'Buscando dispositivo...'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 200, // Establece el tamaño deseado del CircularProgressIndicator
+                height: 200, // Establece el tamaño deseado del CircularProgressIndicator
+                child: CircularProgressIndicator(
+                  strokeWidth: 15, // Ajusta el ancho del indicador circular
+                  valueColor: AlwaysStoppedAnimation<Color>(Color(0xFF00A897)),
+                ),
+              ),
+              SizedBox(height: 80),
+              Text('Connecting...', style: TextStyle(color: Color(0xFF00A897), fontSize: 40)),
+            ],
+          ),
         ),
       ),
     );
