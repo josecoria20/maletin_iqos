6ee26424f5676a38f6a67536bccc7fc8a42131d5
felipeflourwears <A01274880@tec.mx>
2024-12-27 17:06:50 -0600

    Update range of Sensor with 10K resistence

--- ESP32Module/ESP32Connection/ESP32Connection.ino
+++ ESP32Module/ESP32Connection/ESP32Connection.ino
@@ -1,14 +1,13 @@
-// ************ LIBRERÍAS ************////
-#include <ArduinoBLE.h> //Libreria de Bluetoooth 5.0
+#include <ArduinoBLE.h> // Libreria de Bluetooth 5.0
 
-#define L_BLE      2
-int fsrAnalogPin1 = 32;
-int fsrAnalogPin2 = 34;
+#define L_BLE 2
+int fsrAnalogPin1 = 32; // Pin para sensor 1
+int fsrAnalogPin2 = 34; // Pin para sensor 2
 int fsrReading1;
 int fsrReading2;
-String datosAEnviarFSR1 = "2";
-String datosAEnviarFSR2 = "3";
-String datosAEnviar0 = "0";
+String datosAEnviarFSR1 = "3"; // Datos a enviar para el sensor 1 (presionado)
+String datosAEnviarFSR2 = "2"; // Datos a enviar para el sensor 2 (presionado)
+String datosAEnviar0 = "0";    // Datos a enviar cuando no se cumple ninguna condición
 
 bool sentFSR1 = false;
 bool sentFSR2 = false;
@@ -16,13 +15,13 @@ bool sentFSR2 = false;
 unsigned long lastSendTime = 0;
 unsigned long sendInterval = 2500; // Intervalo de tiempo en milisegundos
 
-// ************ CONSTANTES GLOBALES ************
+// Agregar la variable standbyMode
 bool standbyMode = true;
 
 // Bluetooth® Low Energy LED Service
 BLEService ledService("19B10000-E8F2-537E-4F6C-D104768A1214");
 
-// Bluetooth® Low Energy LED Switch Characteristic - custom 128-bit UUID, read and writable by central
+// Bluetooth® Low Energy LED Switch Characteristic
 BLEStringCharacteristic switchCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite, 23);
 
 String payload = "";
@@ -36,37 +35,34 @@ void setup() {
 
 // ************ LOOP ************
 void loop() {
-  BLE.poll();
-  if (!standbyMode) {
-    // Realizar alguna acción cuando no esté en modo standby
-  }
+  BLE.poll();  // Necesario para BLE
 
+  // Leer valores de los sensores
   fsrReading1 = analogRead(fsrAnalogPin1);
   fsrReading2 = analogRead(fsrAnalogPin2);
 
+  Serial.print("FSR1: ");
+  Serial.println(fsrReading1);
+  Serial.print("FSR2: ");
+  Serial.println(fsrReading2);
 
-
-  //Serial.print("FSR1: ");
-  //Serial.println(fsrReading1);
-  //Serial.print("FSR2: ");
-  //Serial.println(fsrReading2);
-
-  if (fsrReading2 <= 5 && !sentFSR2) {
+  // Delimitar los valores de los sensores antes de enviar
+  if (fsrReading2 >= 3000 && fsrReading2 <= 4095 && !sentFSR2) {
     switchCharacteristic.setValue(datosAEnviarFSR2.c_str());
-    Serial.println("-------------------------------------------------------------Enviado 3 para FSR2-------------------------------------------------------------");
+    Serial.println("Enviado 3 para FSR2");
     sentFSR2 = true;
     lastSendTime = millis(); // Actualizar el tiempo del último envío
-  } else if (fsrReading2 > 5) {
+  } else if (fsrReading2 < 3000 || fsrReading2 > 4095) {
     sentFSR2 = false;
   }
-  
-  // Verificar condiciones para enviar datos
-  if (fsrReading1 <= 5 && !sentFSR1) {
+
+  // Delimitar el rango del sensor 1
+  if (fsrReading1 >= 3000 && fsrReading1 <= 4095 && !sentFSR1) {
     switchCharacteristic.setValue(datosAEnviarFSR1.c_str());
-    Serial.println("-------------------------------------------------------------Enviado 2 para FSR1-------------------------------------------------------------");
+    Serial.println("Enviado 2 para FSR1");
     sentFSR1 = true;
     lastSendTime = millis(); // Actualizar el tiempo del último envío
-  } else if (fsrReading1 > 5) {
+  } else if (fsrReading1 < 3000 || fsrReading1 > 4095) {
     sentFSR1 = false;
   }
 
@@ -74,35 +70,24 @@ void loop() {
   if (millis() - lastSendTime >= sendInterval) {
     // Enviar 0 después del intervalo de tiempo especificado
     switchCharacteristic.setValue(datosAEnviar0.c_str());
-    Serial.println("-------------------------------------------------------------Enviado 0-------------------------------------------------------------");
+    Serial.println("Enviado 0");
   }
-
 }
 
 // ************ HANDLERS BLE ************
-  void characteristicUpdatedHandler(BLEDevice central, BLECharacteristic characteristic){
-    payload = switchCharacteristic.value();
-    Serial.println("Valor recibido: " + payload);  
+void characteristicUpdatedHandler(BLEDevice central, BLECharacteristic characteristic) {
+  payload = switchCharacteristic.value();
+  Serial.println("Valor recibido: " + payload);
 
-    // Verificar si el valor recibido no está vacío
-    if (payload.length() > 0) {
-      // Realizar alguna acción basada en el valor recibido
-      // Por ejemplo, enviar los datos recibidos directamente a través del monitor serial
-      Serial.println("Datos recibidos: " + payload);
-
-      // Poner en standby mode nuevamente después de recibir una actualización
-      standbyMode = true;
-
-      // Actualizar la característica BLE con los datos recibidos
-      switchCharacteristic.setValue(payload);
-    } else {
-      // Si el valor recibido está vacío, imprimir un mensaje de error
-      Serial.println("Error: No se recibieron datos.");
-    }
-
-    // Realizar alguna acción cuando se actualiza la característica BLE
-    standbyMode = true; // Poner en standby mode nuevamente después de recibir una actualización
+  if (payload.length() > 0) {
+    Serial.println("Datos recibidos: " + payload);
+    standbyMode = true;
+    switchCharacteristic.setValue(payload);
+  } else {
+    Serial.println("Error: No se recibieron datos.");
   }
+  standbyMode = true; // Volver al modo standby
+}
 
 void connectHandler(BLEDevice central) {
   Serial.print("Dispositivo BLE conectado, dirección: ");
@@ -111,32 +96,26 @@ void connectHandler(BLEDevice central) {
 }
 
 void disconnectHandler(BLEDevice central) {
-  // Realizar alguna acción cuando se desconecta un dispositivo BLE
   digitalWrite(L_BLE, LOW);
 }
 
 // ************ FUNCIONES ************
-// Esta función realiza todas las configuraciones necesarias para inicializar el módulo BLE
+// Inicializa el módulo BLE
 void iniciarBLE() {
-  // Iniciar inicialización
-  if(!BLE.begin()){
+  if (!BLE.begin()) {
     Serial.println("Iniciación del módulo BLE fallida");
-    while(1);
+    while (1);
   }
-  BLE.setLocalName("ESP32IQOS"); // Se le asigna un nombre al dispositivo periferico 
-  BLE.setAdvertisedService(ledService); // Se le asignan los servicios correspondientes
-  ledService.addCharacteristic(switchCharacteristic); // Agrega la característica al servicio
-  BLE.addService(ledService); // Agregar el servicio
+  BLE.setLocalName("ESP32IQOS");
+  BLE.setAdvertisedService(ledService);
+  ledService.addCharacteristic(switchCharacteristic);
+  BLE.addService(ledService);
 
-  // assign event handlers for connected, disconnected to peripheral
   BLE.setEventHandler(BLEConnected, connectHandler);
   BLE.setEventHandler(BLEDisconnected, disconnectHandler);
-  
-  // assign event handlers for characteristics:  
+
   switchCharacteristic.setEventHandler(BLEUpdated, characteristicUpdatedHandler);
 
-  BLE.advertise(); // Inicia el Advertising
-  Serial.println("BLE LED Peripheral"); // Se imprime en monitor la correcta inicializacion
+  BLE.advertise();
+  Serial.println("BLE LED Peripheral");
 }
-
-
